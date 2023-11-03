6fc9543a0ddc0c09404d664e97e5b511c13b3b38
juanpabarriosr <32270243+juanpabarriosr@users.noreply.github.com>
2023-11-03 10:15:23 -0500

    TalleresAdapter onCreateViewHolder

--- app/src/main/java/es/repositoriocompartido/psicologiaapp/TalleresAdapter.kt
+++ app/src/main/java/es/repositoriocompartido/psicologiaapp/TalleresAdapter.kt
@@ -1,15 +1,19 @@
 package es.repositoriocompartido.psicologiaapp
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class TalleresAdapter(private val talleres:List<String>) : RecyclerView.Adapter<TalleresViewHolder>(){
+
+    //Se pinta un elemento o vista dentro del Recyclerview
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TalleresViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_taller,parent,false)
+        return TalleresViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return talleres.size
     }
 
     override fun onBindViewHolder(holder: TalleresViewHolder, position: Int) {
