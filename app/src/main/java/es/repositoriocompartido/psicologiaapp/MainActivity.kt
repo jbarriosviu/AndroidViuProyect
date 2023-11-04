177d6ff7482d32deabfa20a875f7499b27f2c4e5
juanpabarriosr <32270243+juanpabarriosr@users.noreply.github.com>
2023-11-04 02:47:12 -0500

    Activity Main ok

---
+++ app/src/main/java/es/repositoriocompartido/psicologiaapp/MainActivity.kt
@@ -0,0 +1,104 @@
+package es.repositoriocompartido.psicologiaapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.slider.RangeSlider
+
+class MainActivity : AppCompatActivity() {
+
+    //Variables de calculo  o funcionamiento
+    private var hombreSeleccionado:Boolean = true
+    private var edad:Int = 18
+    private var hijos:Int = 0
+
+    //Componentes
+    private lateinit var cvHombre: CardView
+    private lateinit var cvMujer: CardView
+    private lateinit var txtEdad: TextView
+    private lateinit var rngEdad: RangeSlider
+    private lateinit var txtHijos: TextView
+    private lateinit var btnMasHijos: FloatingActionButton
+    private lateinit var btnMenosHijos: FloatingActionButton
+    private lateinit var edtSueldo: EditText
+    private lateinit var btnDescuento: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Funcion de inicializacion de componentes
+        initComponentes()
+        //Funcion de escucha de componentes
+        initListener()
+    }
+
+    private fun initComponentes(){
+        cvHombre = findViewById(R.id.cvHombre)
+        cvMujer = findViewById(R.id.cvMujer)
+
+        txtEdad = findViewById(R.id.txtEdad)
+        txtEdad.text = "${edad.toString()} años"
+        rngEdad = findViewById(R.id.rngEdad)
+
+        txtHijos = findViewById(R.id.txtHijos)
+        txtHijos.text = hijos.toString()
+        btnMasHijos = findViewById(R.id.btnMasHijos)
+        btnMenosHijos = findViewById(R.id.btnMenosHijos)
+
+        btnDescuento = findViewById(R.id.btnDescuento)
+
+    }
+
+    private fun initListener(){
+        //CardView
+        cvHombre.setOnClickListener{cambiarColorSexo(true)}
+        cvMujer.setOnClickListener{cambiarColorSexo(false)}
+
+        //Listener de edad
+        rngEdad.addOnChangeListener { slider, value, fromUser ->
+            edad = value.toInt()
+            txtEdad.text = "${edad.toString()} años"
+        }
+
+        //Listener de Peso
+        btnMasHijos.setOnClickListener {
+            if(hijos<12) hijos += 1
+            setHijo()
+        }
+        btnMenosHijos.setOnClickListener {
+            if(hijos>0) hijos -= 1
+            setHijo()
+        }
+
+
+//        //Calculo del IMC
+//        btnCalculate.setOnClickListener{
+//            val imc:Double = calcularIMC()
+//            val intent = Intent(this,ResultImcMejoradoActivity::class.java)
+//            intent.putExtra("IMC",imc)
+//            startActivity(intent)
+//        }
+    }
+
+    fun cambiarColorSexo(esHombre:Boolean){
+        if(esHombre){
+            cvMujer.setCardBackgroundColor(ContextCompat.getColor(this,R.color.card_color_no_selected))
+            cvHombre.setCardBackgroundColor(ContextCompat.getColor(this,R.color.card_color))
+            hombreSeleccionado = false
+        }else{
+            cvMujer.setCardBackgroundColor(ContextCompat.getColor(this,R.color.card_color))
+            cvHombre.setCardBackgroundColor(ContextCompat.getColor(this,R.color.card_color_no_selected))
+            hombreSeleccionado = true
+        }
+    }
+
+    private fun setHijo() {
+        txtHijos.text = hijos.toString()
+    }
+}
